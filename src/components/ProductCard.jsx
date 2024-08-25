10021616a9de80c14ca907a0b14d2b1b650800b9
Mohammed <engmo159@git.com>
2024-08-25 19:27:16 +0300

    sadasdsa

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from '@material-tailwind/react'
+import { Rating, ThinStar } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
+const myStyles = {
+  itemShapes: ThinStar,
+  activeFillColor: '#ffb700',
+  inactiveFillColor: '#fbf1a9',
+}
+const ProductCard = ({
+  title,
+  description,
+  price,
+  discountPercentage,
+  rating,
+  thumbnail,
+}) => {
+  const [starRating, setStarRating] = useState(Math.round(rating))
+
+  return (
+    <Card className='flex items-center justify-between bg-gray-300 cursor-pointer hover:scale-105 transition-all'>
+      <CardHeader color='blue-gray' className='relative mt-1 '>
+        <img src={thumbnail} className='w-full' />
+      </CardHeader>
+      <CardBody className='p-2 flex flex-col gap-2 items-center'>
+        <Typography variant='h5' color='black' className='text-md'>
+          {title}
+        </Typography>
+        <Typography
+          variant='paragraph'
+          color='gray'
+          className='text-xs text-center '
+        >
+          {description.slice(0, 70)}
+        </Typography>
+        <div className='flex justify-evenly items-center w-full'>
+          <Typography
+            variant='h6'
+            color='gray'
+            className='font-bold line-through text-lg'
+          >
+            {price}$
+          </Typography>
+          <Typography variant='h6' color='green' className='font-bold text-lg'>
+            {((price * (100 - discountPercentage)) / 100).toFixed(2)}$
+          </Typography>
+        </div>
+        <Rating
+          itemStyles={myStyles}
+          style={{ maxWidth: 180 }}
+          value={starRating}
+          onChange={setStarRating}
+          transition='zoom'
+        />
+      </CardBody>
+
+      <CardFooter className='p-0 w-full '>
+        <Button className='w-full rounded-none  bg-teal-700 text-md'>
+          Add to cart
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default ProductCard
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard'
+
 const Products = () => {
-  return <div className='text-center'>Products</div>
+  const [products, setProducts] = useState([
+    {
+      id: 0,
+      title: '',
+      description: '',
+      price: 0,
+      discountPercentage: 0,
+      rating: 0,
+      thumbnail: '',
+    },
+  ])
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(res => res.json())
+      .then(data => setProducts(data.products))
+  }, [])
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mx-2 mt-2'>
+      {products.map(
+        ({
+          id,
+          title,
+          description,
+          price,
+          discountPercentage,
+          rating,
+          thumbnail,
+        }) => (
+          <ProductCard
+            key={id}
+            title={title}
+            description={description}
+            price={price}
+            discountPercentage={discountPercentage}
+            rating={rating}
+            thumbnail={thumbnail}
+          />
+        )
+      )}
+    </div>
+  )
 }
 
 export default Products
