568011f6ca05f728f7fa88c8c0ed8ace308be786
Mohammed <engmo159@git.com>
2024-08-30 17:00:13 +0300

    vvcc

--- src/UserLayout.jsx
+++ src/UserLayout.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
-import Products from './pages/products'
+import Products from './pages/Products'
 import Contact from './pages/contact'
 import LogIn from './pages/login'
 import SignUp from './pages/SignUp'
