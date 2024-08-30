31b5102663979735e954a7b344bfbea8cd7b8882
Mohammed <engmo159@git.com>
2024-08-30 17:02:45 +0300

    ada

--- src/UserLayout.jsx
+++ src/UserLayout.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import Contact from './pages/Contact'
-import LogIn from './pages/Login'
+import LogIn from './pages/LogIn'
 import SignUp from './pages/SignUp'
 import NotFound from './pages/NotFound'
 
