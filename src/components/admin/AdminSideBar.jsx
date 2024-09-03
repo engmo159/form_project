0499a5b9bf3af91d0435cc048cc6040dc0ccf149
Mohammed <engmo159@git.com>
2024-09-03 21:53:10 +0300

    efew

--- src/components/admin/AdminSideBar.jsx
+++ src/components/admin/AdminSideBar.jsx
@@ -4,7 +4,7 @@ const AdminSideBar = () => {
   return (
     <div>
       <div className='fixed top-18 z-10 h-full text-center text-white font-bold text-2xl  bg-teal-700 py-8 w-1/6 '>
-        <h1>DashBoard</h1>
+        <h1 className='text-black'>DashBoard</h1>
         <ul className='flex flex-col items-center justify-evenly h-2/3 '>
           <li className='hover:text-black transition-all'>
             <Link to={'/admin/products'}>Products</Link>
